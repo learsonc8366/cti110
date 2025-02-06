bbc8d22f3a697735ef9be8bb95cfba7a054e6c41
learsonc8366 <learsonc8366@student.faytechcc.edu>
2025-02-06 14:21:32 -0500

    Add files via upload

---
+++ P1LAB1_LearsonCaylee.py
@@ -0,0 +1,12 @@
+ # Caylee Learson
+ # 2-06-2025
+ # P1LAB1
+ # This program will demo python code
+
+fname = input("Enter your first name:")
+
+lname = input("Ener your last name:")
+
+print("Hello, " + str(fname) +" " + str(lname) +"! Welcome to CTI-110")
+
+print("Hello,",fname, str(lname) +"! Welcome to CTI-110")
