3064ba643c67b6eca98220a9c3e12399da5c7823
learsonc8366 <learsonc8366@student.faytechcc.edu>
2025-03-25 13:28:02 -0400

    Add files via upload

---
+++ P4Lab2.py
@@ -0,0 +1,7 @@
+keep_going = 'yes'
+total = 0
+while keep_going.lower() == 'yes':
+    num = int(input("Enter a number: "))
+    total += num
+    keep_going = input("Do you want to enter more numbers? Enter yes or no: ")
+print(total)
